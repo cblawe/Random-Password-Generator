ea3a6325effec1a0ef3250b2c2f1b442769c1d35
cblawe <67339121+cblawe@users.noreply.github.com>
2022-11-16 14:30:16 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let firstPass = document.getElementById("first-pass")
+let secondPass = document.getElementById("second-pass")
+
+//password should be 15 characters long
+//using random from const characters
+
+function generatePass() {  
+    firstPass.textContent = ""
+    secondPass.textContent = ""
+    for (let i = 0; i < 15; i++) {
+        firstRandom = Math.floor(Math.random()*characters.length)
+        firstPass.textContent += characters[firstRandom]
+        
+        secondRandom = Math.floor(Math.random()*characters.length)
+        secondPass.textContent += characters[secondRandom]   
+    } 
+}
+
+function copyPass1() {
+    navigator.clipboard.writeText(firstPass.textContent)
+    alert("Copied: " + firstPass.textContent)
+}
+
+function copyPass2() {
+    navigator.clipboard.writeText(secondPass.textContent)
+    alert("Copied: " + secondPass.textContent)
+}
